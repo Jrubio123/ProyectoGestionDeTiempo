827a8b8e9ea8a38190f985a7cd517e0ee2cd718f
Juan Pablo Rubio Mejía <juan.rubio@silverconsulting.com.co>
2026-02-11 11:03:11 -0500

    Supabase obliga a usar una conexión segura (SSL)

--- back/src/index.js
+++ back/src/index.js
@@ -48,7 +48,8 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 function buildTotalLetras(numero, moneda = 'COP') {

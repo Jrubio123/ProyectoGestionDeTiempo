944897fcc7fe47c498ee1859ff24230054fec0aa
Juan Pablo Rubio Mejía <juan.rubio@silverconsulting.com.co>
2026-02-26 13:44:24 -0500

    Quitar envío de tamaño de firma en mis cuentas de cobro

--- front/js/mis-cuentas-cobros.js
+++ front/js/mis-cuentas-cobros.js
@@ -1,7 +1,6 @@
 // js/mis-cuentas-cobros.js
 window.misCuentasApp = function () {
     const API = window.API_BASE || "http://localhost:4000";
-    const SIGNATURE_SIZE_PERCENT = 20;
 
     return {
         usuario: { id: null },
@@ -148,9 +147,7 @@ window.misCuentasApp = function () {
             if (!confirmar) return;
 
             try {
-                const res = await axios.post(`${API}/cuentas-cobro/${cuenta.id}/firma/iniciar`, {
-                    signature_size_percent: SIGNATURE_SIZE_PERCENT
-                });
+                const res = await axios.post(`${API}/cuentas-cobro/${cuenta.id}/firma/iniciar`);
                 const urlFirma = res?.data?.url_firma || "";
                 if (urlFirma) {
                     window.open(urlFirma, "_blank", "noopener");

a9897c97628bb1ea0e72e7af49d9eaa10ab2eb48
Juan Rubio <juanpablo.rubio990221@gmail.com>
2026-03-16 20:52:05 -0500

    Reemplazar CDN de Tailwind y agregar build CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./front/**/*.html",
+    "./front/**/*.js"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
